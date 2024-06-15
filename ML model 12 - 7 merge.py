ba3f7738a9be4e846e6835f8c3f442086ecce04b
IkshwakJ <ikshwakjinesh@gmail.com>
2024-06-15 10:13:22 -0400

    Create ML model 12 - 7 merge.py

---
+++ ML model 12 - 7 merge.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import matplotlib
+moment_data = pd.read_csv("Moment_data_model_9.csv")
+print(moment_data.describe())
+response = 'knee_angle_r_moment'
+y = moment_data[[response]]
+print(y)
+predictors = list(moment_data.columns)
+predictors.remove(response)
+predictors.remove('time')
+predictors.remove('time_prev')
+predictors.remove('time_prev_prev')
+predictors.remove('delta_t')
+predictors.remove('delta_t_prev')
+predictors.remove('del_delta_t')
+# predictors.remove('knee_angle_r_prev')
+predictors.remove('knee_angle_r_prev_prev')
+# predictors.remove('ankle_angle_r_prev')
+predictors.remove('ankle_angle_r_prev_prev')
+# predictors.remove('knee_angle_l_prev')
+predictors.remove('knee_angle_l_prev_prev')
+# predictors.remove('ankle_angle_l_prev')
+predictors.remove('ankle_angle_l_prev_prev')
+# predictors.remove('ankle_angle_r_moment')
+predictors.remove('knee_angle_l_moment')
+predictors.remove('ankle_angle_l_moment')
+predictors.remove('knee_angular_vel_r_prev')
+# predictors.remove('knee_angular_vel_r')
+predictors.remove('ankle_angular_vel_r_prev')
+# predictors.remove('ankle_angular_vel_r')
+predictors.remove('knee_angular_vel_l_prev')
+# predictors.remove('knee_angular_vel_l')
+predictors.remove('ankle_angular_vel_l_prev')
+# predictors.remove('ankle_angular_vel_l')
+x = moment_data[predictors]
+print(x)
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, random_state = 1234) 
+from sklearn.linear_model import LinearRegression
+model = LinearRegression().fit(x_train, y_train)
+print("Model Intercept: ", model.intercept_)
+print("Coeff Values: ", model.coef_)
+print("Score: ", model.score(x_test,y_test))
+y_pred = model.predict(x_test)
+from sklearn.metrics import mean_absolute_error
+print("Mean Squared Error", mean_absolute_error(y_test, y_pred))
